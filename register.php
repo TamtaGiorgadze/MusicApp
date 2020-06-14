cdcfcf7470780df927397b169937f4b1ab556cdd
TamtaGiorgadze <tamt.giorgadze@gmail.com>
2020-06-14 17:37:05 +0400

    refactoring error messages

--- register.php
+++ register.php
@@ -8,6 +8,12 @@
     include("includes/handlers/register-handler.php");
     include("includes/handlers/login-handler.php");
 
+    function getInputValue($name) {
+        if(isset($_POST[$name])) {
+            echo $_POST[$name];
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -37,27 +43,27 @@
             <p>
                 <?php echo $account->getError(Constants::$usernameCharacters); ?>
                 <label for="username">Username</label>
-                <input id="username" name="username" type="text" placeholder="Enter Your Username" required>
+                <input id="username" name="username" type="text" placeholder="Enter Your Username" value="<?php getInputValue("username"); ?>" required>
             </p>
             <p>
                 <?php echo $account->getError(Constants::$firtnameCharacters); ?>
                 <label for="firstName">First Name</label>
-                <input id="firstName" name="firstName" type="text" placeholder="Enter Your First Name" required>
+                <input id="firstName" name="firstName" type="text" placeholder="Enter Your First Name" value="<?php getInputValue("firstName"); ?>" required>
             </p>
             <p>
                 <?php echo $account->getError(Constants::$lastnameCharacters); ?>
                 <label for="lastName">Last Name</label>
-                <input id="lastName" name="lastName" type="text" placeholder="Enter Your Last Name" required>
+                <input id="lastName" name="lastName" type="text" placeholder="Enter Your Last Name" value="<?php getInputValue("lastName"); ?>" required>
             </p>
             <p>
                 <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
                 <?php echo $account->getError(Constants::$emailInvalid); ?>
                 <label for="email">Email</label>
-                <input id="email" name="email" type="text" placeholder="Enter Your Email" required>
+                <input id="email" name="email" type="text" placeholder="Enter Your Email" value="<?php getInputValue("email"); ?>" required>
             </p>
             <p>
                 <label for="email2">Confirm Email</label>
-                <input id="email2" name="email2" type="email" placeholder="Confirm Your Email" required>
+                <input id="email2" name="email2" type="email" placeholder="Confirm Your Email" value="<?php getInputValue("email2"); ?>" required>
             </p>
             <p>
                 <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
